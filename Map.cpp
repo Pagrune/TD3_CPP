9ea87509915a23d66ea1444828c0bc764a9f448b
Pagrune <pauline.grunewald2@gmail.com>
2025-11-17 14:41:36 +0100

    Map compter nombre d'occurence int manière brute et optimisée

---
+++ Map.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+void countFrequenciesBrutForce(const vector<int>& numbers){
+       set<int> printed;  // garde les nombres déjà affichés
+
+    for(size_t i = 0; i < numbers.size(); i++){
+        if(printed.find(numbers[i]) != printed.end()) {
+            continue; // déjà compté, on passe
+        }
+
+        int count = 0;
+        for(size_t j = 0; j < numbers.size(); j++){
+            if(numbers[i] == numbers[j]){
+                count++;
+            }
+        }
+
+        cout << numbers[i] << ": " << count << " times" << endl;
+        printed.insert(numbers[i]); // marque ce nombre comme affiché
+    }
+}
+
+map<int, int> countFrequenciesOptimal(const vector<int>& numbers){
+    map<int, int> frequencyMap;
+    for(int number : numbers){
+        frequencyMap[number]++;
+    }
+    return frequencyMap;
+}
+
+int main(){
+    vector<int> numbers = {1, 2, 3, 2, 4, 1, 5, 5, 6};
+
+    cout << "Frequeny (Brute Force):" << endl;
+    countFrequenciesBrutForce(numbers);
+
+    cout << "Frequency (Optimal):" << endl;
+    map<int, int> FrequencyMapOptimal = countFrequenciesOptimal(numbers);
+    for(const auto& entry : FrequencyMapOptimal){
+        cout << entry.first << ": " << entry.second << "times" << endl;
+    }
+
+    return 0;
+}
